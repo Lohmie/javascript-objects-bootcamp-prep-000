3ab4ab2eb45f31d7f2785c4be871a2baf47f46ee
Mariam <mariamjhabib@gmail.com>
2019-03-17 19:04:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { playlist: Object };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  meals.snack = 'yogurt'
+  playlist.artistName = 'songTitle';
   return playlist;
 }
