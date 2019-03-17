923ca6113dea489e5dcd40315d59e2e2e1183dd5
Mariam <mariamjhabib@gmail.com>
2019-03-17 21:06:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,9 +2,15 @@ var playlist = {Baa: "song"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
  
- playlist[artistName] = songTitle;
+  playlist[artistName] = songTitle;
 
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+
+  delete playlist.artistName;
+  
+  return playlist;
+  
+}
